f43db2938041bce8a1d83537d598457c92c86f9a
teguhsms05 <teguhsms05@gmail.com>
2022-06-24 14:50:28 +0700

    update 6

--- src/app.py
+++ src/app.py
@@ -135,4 +135,4 @@ api.add_resource(Players, "/api", methods=["GET", "POST"])
 
 if __name__ == "__main__":
     #app.run(debug=True, host="0.0.0.0")
-    app.run(debug=True, host="0.0.0.0", port=5005)
+    app.run(debug=True, host="0.0.0.0")
